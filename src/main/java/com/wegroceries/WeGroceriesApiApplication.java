f0c46e523f599673109d0acb6a19fe8f64092f84
Amarachi <amyewulonu@gmail.com>
2025-01-20 11:44:53 +0000

    First commit

---
+++ src/main/java/com/wegroceries/WeGroceriesApiApplication.java
@@ -0,0 +1,13 @@
+package com.wegroceries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeGroceriesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeGroceriesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wegroceries/WeGroceriesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wegroceries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeGroceriesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
