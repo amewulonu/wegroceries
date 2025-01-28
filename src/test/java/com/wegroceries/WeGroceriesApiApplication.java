d19e70025f4ddb1bc77da5ca878ab665b05bbf93
Amarachi <amyewulonu@gmail.com>
2025-01-28 22:40:13 +0000

    changes made to wegroceriesaplication

--- src/test/java/com/wegroceries/WeGroceriesApiApplication.java
+++ src/test/java/com/wegroceries/WeGroceriesApiApplication.java
@@ -2,9 +2,12 @@ package com.wegroceries;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class WeGroceriesApiApplicationTests {
+@ActiveProfiles("test")
+
+public class WeGroceriesApiApplication {
 
 	@Test
 	void contextLoads() {
